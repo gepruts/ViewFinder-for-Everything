f7b4709f69ffb05afe2412c72b8f4cc341761a26
gepruts <66734443+gepruts@users.noreply.github.com>
2026-01-13 19:29:01 +0100

    Add files via upload
    Added search-as-you-type

---
+++ HTML/ViewFinder.js
@@ -0,0 +1,29 @@
+//	Put a marker on the map on this location
+//	const HOME_LOCATION = {lat:-32.653080950980105, lng: 138.09947636484688};
+
+//	Logging on/off.
+//	const DEBUG = 1;
+
+//	Zoomlevel on map per category
+	const CITY_ZOOM = 12;
+	const COUNTRY_ZOOM = 6;
+	const CONTINENT_ZOOM = 4;
+	const DEFAULT_ZOOM = 9;
+	const PREVIEW_ZOOM = 4;
+
+//	Max list entries
+//	const MAX_ROWS  = 10;
+
+//	geonames settings	
+	//const URL      = 'http://api.geonames.org/searchJSON?q=';
+	const URL      = 'http://api.geonames.org/searchJSON?';
+	const UID = 'viewfinder';
+
+//	Minimum #characters before checking database
+	const MIN_QUERYLENGTH_FOR_SEARCH = 4
+
+// 2do: If typing speed (in ms) goes below TYPING_SPEED,
+//	start checking database.
+	const TYPING_SPEED = 300
+
+
--- ViewFinder for Everything.cmd
+++ ViewFinder for Everything.cmd
@@ -1,3 +1,32 @@
 @echo off & setlocal
+pushd "%~dp0"
 
-start "" msedge --NEWWINDOW --APP="%~dp0\ViewFinder for Everything.html"
+set OUTFILE=.\HTML\UserSettings.js
+
+
+set HOME=
+set DEBUG=
+set MAX_ROWS=
+
+echo.> "%OUTFILE%"
+
+
+:: Convert INI settings to JavaScript
+
+	for /f "usebackq delims=" %%x in ("ViewFinder.ini") do (set %%x 2>nul)
+
+	(
+		If defined HOME (
+			for /f "tokens=1,2 delims=, " %%x in ("%HOME%") do (
+				echo const HOME_LOCATION = {lat:%%x, lng:%%y};)
+		)
+
+		If defined DEBUG echo const DEBUG = %DEBUG%;
+		If defined MAX_ROWS echo const MAX_ROWS = %MAX_ROWS%;
+		If defined ANIMATED_ZOOM echo const ANIMATED_ZOOM = %ANIMATED_ZOOM%;
+		
+	) > "%OUTFILE%"
+
+
+
+start "" msedge --NEWWINDOW --APP="%~dp0\HTML\ViewFinder for Everything.html"
